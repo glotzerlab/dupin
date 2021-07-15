41c8c4e0a88725afae76b6aea7e58896d7ba3f9f
Brandon Butler <butlerbr@umich.edu>
2021-07-15 12:22:34 -0400

    Create preprocessing submodule (reorganize others)
    preprocessing handles all signal preprocessing.

--- event_detection/offline/__init__.py
+++ event_detection/offline/__init__.py
@@ -1,3 +1,2 @@
 """Offline methods for event detection in molecular simulations."""
 from . import costs, detect, smoothing, supervised
-from .aggegate import SignalAggregator
---
+++ event_detection/preprocessing/__init__.py
@@ -0,0 +1,4 @@
+"""Perform various kinds of preprocessing on generated signals."""
+
+
+from . import signal, supervised
