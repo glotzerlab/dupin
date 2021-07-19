1da16c0ab088905a80196040639d2d034cb6d841
Brandon Butler <butlerbr@umich.edu>
2021-07-19 11:45:15 -0400

    Add Tee DataMap

--- event_detection/data/map.py
+++ event_detection/data/map.py
@@ -1,5 +1,7 @@
 """`data.base.DataMap` subclasses to transform distributional data."""
 
+from typing import Callable, Dict, List, Union
+
 import numpy as np
 
 from . import base
@@ -35,3 +37,35 @@ class Identity(base.DataMap):
             value.
         """
         return {None: data}
+
+
+class Tee(base.DataMap):
+    """Enable mutliple maps to act on the same generator like object."""
+
+    def __init__(
+        self,
+        generator: base.GeneratorLike,
+        maps: List[Callable[[base.GeneratorLike], base.DataMap]],
+    ):
+        """Create a data.reduce.Tee object.
+
+        Parameters
+        ----------
+        generator: GeneratorLike
+            A generator like object to map to another distribution.
+        reducers: list[callable[base.GeneratorLike, base.DataReducer]]
+            A sequence of callables that take a generator like object and
+            returns a data map. Using the ``wraps`` class method with a
+            `DataMap` subclass is a useful combination.
+        """
+        self._maps = [map_(generator) for map_ in maps]
+        super().__init__(generator)
+
+    def compute(
+        self, distribution: np.typing.ArrayLike
+    ) -> Dict[str, Union[float, np.ndarray]]:
+        """Run all composed map computes."""
+        processed_data = {}
+        for map_ in self._maps:
+            processed_data.update(map_.compute(distribution))
+        return processed_data
