661778c00d01258747b3137de7fd629918a9e4b4
Brandon Butler <butlerbr@umich.edu>
2021-07-19 13:05:35 -0400

    Add new modules to data/__init__.py (and remove old)

--- event_detection/data/__init__.py
+++ event_detection/data/__init__.py
@@ -1,4 +1,4 @@
 """Classes and functions for generating and recording system descriptors."""
-from . import reduce
-from .base import DataMap, DataReducer, Generator
+from . import map, reduce
+from .base import DataMap, DataReducer, Generator, GeneratorLike
 from .freud import FreudDescriptor
