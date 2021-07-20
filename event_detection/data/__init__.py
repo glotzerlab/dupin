a706ded99027ccbbff75f30265611bfbad0586c8
Brandon Butler <butlerbr@umich.edu>
2021-07-20 11:17:14 -0400

    Do not directly import submodules of data into __init__.py
    This is to help with the structure ofthe package for new users.

--- event_detection/data/__init__.py
+++ event_detection/data/__init__.py
@@ -1,4 +1,2 @@
 """Classes and functions for generating and recording system descriptors."""
-from . import map, reduce
-from .base import DataMap, DataReducer, Generator, GeneratorLike
-from .freud import FreudDescriptor
+from . import base, freud, map, reduce
