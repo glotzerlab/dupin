aabb5fc09dab8672555135c8cc20443d2fdd0ff6
Brandon Butler <butlerbr@umich.edu>
2021-07-20 11:36:42 -0400

    Import classes/functions directly into detect.online

--- event_detection/detect/offline/__init__.py
+++ event_detection/detect/offline/__init__.py
@@ -1,2 +1,8 @@
 """Offline methods for event detection in molecular simulations."""
-from . import costs, detect
+
+from .costs import CostLinearFit
+from .detect import (
+    SweepDetector,
+    kneedle_elbow_detection,
+    two_pass_elbow_detection,
+)
