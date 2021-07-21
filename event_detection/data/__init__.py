ffe6b7a4861ac3fde4e5d2fecf1d7654d4be69c1
Brandon Butler <butlerbr@umich.edu>
2021-07-21 11:54:55 -0400

    Directly import SignalAggregator into data

--- event_detection/data/__init__.py
+++ event_detection/data/__init__.py
@@ -1,2 +1,3 @@
 """Classes and functions for generating and recording system descriptors."""
-from . import base, freud, map, reduce
+from . import base, freud, map, reduce, spatial
+from .aggregate import SignalAggregator
