608f81cd4d1a58526cc40c98e9d1503ae8f43ad3
Brandon Butler <butlerbr@umich.edu>
2021-07-23 17:39:30 -0400

    Completely rework StdDevDetector to new API

--- event_detection/detect/online/std_dev.py
+++ event_detection/detect/online/std_dev.py
@@ -1,44 +1,10 @@
 """Use the standard deviation of signals from a baseline to detect events."""
 
-from copy import copy
-from typing import Dict, List, Tuple, Union
-
-import numpy as np
+from typing import Any, Dict
 
 import event_detection.data as data
 
 from . import Detector, DetectorStatus
-
-
-class _SignalStats:
-    def __init__(self) -> None:
-        self._baseline = []
-        self.active_counter = 0
-        self._recompute_stats = True
-
-    def update_baseline(self, entry: float) -> None:
-        self._baseline.append(entry)
-        self._recompute_stats = True
-
-    def compute(self):
-        baseline = np.array(self._baseline)
-        self._mean = np.mean(baseline)
-        self._std_dev = np.std(baseline)
-
-    def __getitem__(self, index: Union[int, slice]):
-        return copy(self._baseline[index])
-
-    @property
-    def mean(self) -> float:
-        if self._recompute_stats:
-            self.compute()
-        return self._mean
-
-    @property
-    def std_dev(self) -> float:
-        if self._recompute_stats:
-            self.compute()
-        return self._std_dev
 
 
 class StdDevDetector(Detector):
@@ -57,11 +23,15 @@ class StdDevDetector(Detector):
         triggering an event.
     status: DetectorStatus
         The current status of the detector.
+    events: list[tuple[int, list[str]]]
+        A list where each entry is a "verified" event with the first entry in
+        the tuple representing the n-th call where the event was detected and
+        the second is a list of feature names that triggered the detector.
     """
 
     def __init__(
         self,
-        generators: List[data.base.Generator],
+        generator: [data.base.GeneratorLike],
         n_training: int = 50,
         n_std_dev: float = 2.0,
         threshold: int = 10,
@@ -83,91 +53,82 @@ class StdDevDetector(Detector):
             The number of consequative calls that a signal is over/under the set
             number of standard deviations.
         """
-        self._generators = generators
+        self._generator = generator
+        self._aggregator = data.SignalAggregator(generator)
         self.n_training = n_training
         self.n_std_dev = n_std_dev
-        self._signals_stats = {}
         self._count = 0
         self._threshold = threshold
-        self._events = []
+        self.events = []
         self.status = DetectorStatus.INACTIVE
 
-    def _get_signals(self, state) -> Dict[str, float]:
-        signals = {}
-        for generator in self.generators:
-            new_signals = generator.generate(state)
-            if signals.keys().isdisjoint(new_signals):
-                signals.update(new_signals)
-            else:
-                raise RuntimeError("All signals must have unique names.")
-        return signals
-
-    def _update_stats(self, signals: Dict[str, float]) -> None:
-        for signal_name, signal in signals.items():
-            self._signals_stats[signal_name].update_baseline(signal)
-
-    def _initialize_stats(self, signals: Dict[str, float]) -> None:
-        for signal_name, signal in signals.items():
-            signal_stats = _SignalStats()
-            signal_stats.update_baseline(signal)
-            self._signals_stats[signal_name] = signal_stats
-
-    def _evaluate_signals(self, signals: Dict[str, float]):
+    def _evaluate_signals(self, snapshot: Dict[str, float]):
         max_event_counter = 0
         confirmed_signals = []
-        for signal_name, signal in signals.items():
-            signal_stats = self._signals_stats[signal_name]
-            deviation = abs(signal_stats.mean - signal)
-            if deviation / signal_stats.std_dev > self.n_std_dev:
-                signal_stats.active_counter += 1
-                if signal_stats.active_counter > max_event_counter:
-                    max_event_counter = signal_stats.active_counter
+        # Check each signal and update confirmed_signals list and all activity
+        # counters
+        for signal_name, signal in snapshot.items():
+            deviation = abs(self._means[signal_name] - signal)
+            if deviation / self._std_dev[signal_name] > self.n_std_dev:
+                current_counter = self._active_counters[signal_name] + 1
+                self._active_counters[signal_name] = current_counter
+                if current_counter > max_event_counter:
+                    max_event_counter = current_counter
+                if current_counter > self._threshold:
                     confirmed_signals.append(signal_name)
             else:
-                signal_stats.active_counter = 0
+                self._active_counters[signal_name] = 0
 
-        if max_event_counter > self._threshold:
+        # Check for detector status
+        if len(confirmed_signals) > 0:
             self.status = DetectorStatus.CONFIRMED
-            self._events.append(self._count, confirmed_signals)
+            self.events.append(self._count, confirmed_signals)
         elif max_event_counter > 0:
             self.status = DetectorStatus.ACTIVE
         else:
             self.status = DetectorStatus.INACTIVE
         return self.status
 
-    def _update_status(self, state) -> DetectorStatus:
-        signals = self._get_signals(state)
-        if self._count == 0:
-            self._initialize_stats(signals)
-            return DetectorStatus.INACTIVE
+    def _update_status(self, *args: Any, **kwargs: Any) -> DetectorStatus:
+        """Update the detector one step with given arguments."""
+        # compute the new index's features
+        self._aggregator.accumulate(*args, **kwargs)
+        # before training is done just return inactive
         if self._count < self.n_training:
-            self._update_stats(signals)
             return DetectorStatus.INACTIVE
-        status = self._evaluate_signals(signals)
+        # Compute std_dev and mean when training is finished
+        if self._count == self.n_training:
+            signals_df = self._aggregator.to_dataframe()
+            self._means = signals_df.mean(axis=1).to_dict()
+            self._std_devs = signals_df.std(axis=1).to_dict()
+            self._active_counters = {name: 0 for name in self._means}
+        # Attempt to detect events pasted training
+        status = self._evaluate_signals(self._aggregator.signals[-1])
         return status
 
-    def update_status(self, state) -> DetectorStatus:
-        """Update the detector status with the given state.
+    def update_status(self, *args: Any, **kwargs: Any) -> DetectorStatus:
+        r"""Update the detector status with the given arguments.
 
         For the first `n_training` calls, only update the baseline for future
         detection.  Afterwards, attempt to detect events.
 
         Parameters
         ----------
-        state: state-like object
-            An object with a `hoomd.Snapshot` like API. Examples include
-            `gsd.hoomd.Snapshot` and `hoomd.Snapshot`. This is used to pass to
-            generator to return the corresponding signals.
+        \*args:
+            positional arguments to pass to ``self.generator.__call__``.
+        \*\*kwargs:
+            keyword arguments to pass to ``self.generator.__call__``.
+
+        Returns
+        -------
+        status: event_detection.detect.online.DetectorStatus
+            The status of the detector (whether an event has been detected).
         """
-        status = self._update_status(state)
+        status = self._update_status(*args, **kwargs)
         self._count += 1
         return status
 
-    def event_details(self) -> List[Tuple[int, List[str]]]:
-        """If an event has been detected provide information about the event."""
-        return self._events
-
     @property
-    def generators(self) -> Tuple[data.base.Generator, ...]:
+    def generator(self) -> data.base.Generator:
         """tuple[data.base.Generator] generators used by the detector."""
-        return self._generators
+        return self._generator
