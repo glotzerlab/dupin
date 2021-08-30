ee97c4867a9007247d8b74e6904bf4c9b90107ed
Brandon Butler <butlerbr@umich.edu>
2021-08-30 18:54:34 -0400

    Add initial support for converting log to DataFrame

--- event_detection/data/logging.py
+++ event_detection/data/logging.py
@@ -1,5 +1,8 @@
 """Functions and classes for allowing logging extra data from pipelines."""
 
+import numpy as np
+import pandas as pd
+
 
 class Logger:
     """Class for logging extra information from data pipeline."""
@@ -28,3 +31,44 @@ class Logger:
     def frame(self):
         """`list` [`dict`]: Assess a particular frame of data."""
         return self._data
+
+    def to_dataframe(self):
+        """Return a `pandas.DataFrame` object consisting of stored data.
+
+        Warning:
+            This assumes the pipeline produces homogenous data along a
+            trajectory.
+        """
+        frame_data = self._data[0]
+        column_index = pd.MultiIndex.from_tuples(
+            _create_column_index(frame_data)
+        )
+        data_arr = _log_data_to_array(
+            self._data,
+            np.empty((len(self._data), len(column_index)), dtype=float),
+        )
+        return pd.DataFrame(data_arr, columns=column_index)
+
+
+def _create_column_index(log_data):
+    for key, value in log_data.items():
+        if isinstance(value, dict):
+            for inner_index in _create_column_index(value):
+                yield (key,) + inner_index
+        else:
+            yield (key,)
+
+
+def _log_data_to_array(data, out):
+    def write_frame(frame_data, out, index):
+        for value in frame_data.values():
+            if isinstance(value, dict):
+                index = write_frame(value, out, index)
+            else:
+                out[index] = value
+                index += 1
+        return index
+
+    for i, frame in enumerate(data):
+        write_frame(frame, out[i], 0)
+    return out
