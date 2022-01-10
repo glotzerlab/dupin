43a3ec44d9867af4b1c4b04f7bb00b0333776a84
Brandon Butler <butlerbr@umich.edu>
2022-01-10 11:02:54 -0500

    Test dupin.preprocessing.supervised

---
+++ tests/preprocessing/test_supervised.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pytest
+import ruptures as rpt
+import sklearn as sk
+
+import dupin as du
+
+
+def test_window_iter():
+    arr = np.arange(1, 21)
+    for i, slice_ in enumerate(du.preprocessing.supervised.window_iter(arr, 3)):
+        assert slice_.mean() == i + 2
+
+
+def test_valid_construction():
+    classifier = sk.tree.DecisionTreeClassifier(max_depth=1)
+    window = du.preprocessing.supervised.Window(classifier, 10, 0.1)
+    assert window.window_size == 10
+    assert window.test_size == 0.1
+    assert window._loss_function == window._default_loss
+    assert window.n_classifiers == 1
+    assert window.store_intermediate_classifiers is False
+    assert window.combine_errors == "mean"
+
+    def loss(cls, x, y):
+        return np.sum(y != cls.predict(x))
+
+    window = du.preprocessing.supervised.Window(
+        classifier, 15, 0.5, loss, True, 10, "median"
+    )
+    assert window.window_size == 15
+    assert window.test_size == 0.5
+    assert window._loss_function == loss
+    assert window.n_classifiers == 10
+    assert window.store_intermediate_classifiers is True
+    assert window.combine_errors == "median"
+
+
+def test_invalid_construction():
+    classifier = sk.tree.DecisionTreeClassifier(max_depth=1)
+    with pytest.raises(ValueError):
+        du.preprocessing.supervised.Window(classifier, 1, 0.1)
+    with pytest.raises(ValueError):
+        du.preprocessing.supervised.Window(classifier, 2, 0.0)
+    with pytest.raises(TypeError):
+        du.preprocessing.supervised.Window(classifier, 2, 0.1, {})
+
+    def loss(cls, x, y):
+        return np.sum(y != cls.predict(x))
+
+    with pytest.raises(TypeError):
+        du.preprocessing.supervised.Window(classifier, 2, 0.1, loss, "foo")
+    with pytest.raises(ValueError):
+        du.preprocessing.supervised.Window(classifier, 2, 0.1, loss, True, 0)
+    with pytest.raises(ValueError):
+        du.preprocessing.supervised.Window(classifier, 2, 0.1, loss, True, -1)
+    with pytest.raises(ValueError):
+        du.preprocessing.supervised.Window(
+            classifier, 2, 0.1, loss, True, 1, "mode"
+        )
+
+
+@pytest.fixture(scope="session", params=(1, 2))
+def signal_with_mean_shift(seeds):
+    return rpt.pw_constant(100, 2, 1, 0.5, delta=(3, 5), seed=seeds())
+
+
+@pytest.fixture
+def window():
+    classifier = sk.tree.DecisionTreeClassifier(max_depth=1)
+    return du.preprocessing.supervised.Window(
+        classifier=classifier, window_size=10, test_size=0.5, n_classifiers=20
+    )
+
+
+def test_compute(window, signal_with_mean_shift):
+    signal, breakpoint = signal_with_mean_shift
+    error = window.compute(signal)
+    assert abs(error.argmin() - breakpoint[0]) < 7
+
+
+@pytest.fixture
+def random_signal(rng):
+    return rng.uniform(-500_000, 500_000, size=20).reshape((-1, 1))
+
+
+def test_error_aggregation_methods(window, random_signal):
+    assert np.allclose(window._reduce(random_signal), np.mean(random_signal))
+    window.combine_errors = "median"
+    assert np.allclose(window._reduce(random_signal), np.median(random_signal))
+
+
+def test_storing_classifiers(window, random_signal):
+    window.store_intermediate_classifiers = True
+    window.n_classifiers = 1
+    window.compute(random_signal)
+    expected_len = len(
+        list(
+            du.preprocessing.supervised.window_iter(
+                random_signal, window.window_size
+            )
+        )
+    )
+    assert len(window.classifiers_) == expected_len
+    assert all(
+        isinstance(classifier, sk.tree.DecisionTreeClassifier)
+        for window in window.classifiers_
+        for classifier in window
+    )
