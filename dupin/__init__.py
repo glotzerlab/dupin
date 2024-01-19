6fe5f0ec0ca8c1935a88fe52ad3ab386fa0f69de
Brandon Butler <butlerbr@umich.edu>
2024-01-19 12:49:34 -0500

    fix: version

--- dupin/__init__.py
+++ dupin/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Brandon Butler"""
 __email__ = "butlerbr@umich.edu"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from . import data, detect, errors, postprocessing, preprocessing
 
